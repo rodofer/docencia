544265ef18c9545f0e52d734cee16dd08dd5edfb
Rodolfo <rodofercl@gmail.com>
2024-09-11 16:16:30 -0300

    ejercicio 1

--- listado-4/1.py
+++ listado-4/1.py
@@ -5,18 +5,18 @@ volver a pedir el número hasta que supere
 la validación
 """
 
-num = int(input("Ingresa un numero: "))
-
-while num < 0:
-    num = int(input("Ingresa un numero: "))
-    print("El numero debe ser positivo")
-
 # Hileok Hernandez
-# while True:
-#     num = int(input('ingrese el numero: '))
+while True:
+    num = int(input('ingrese el numero: '))
 
-#     if num > 0:
-#         print('es numero positivo.')
-#         break
-#     else:
-#         print('no es positivo, vuelva a intentarlo')
+    if num > 0:
+        print('es numero positivo.')
+        break
+    else:
+        print('no es positivo, vuelva a intentarlo')
+
+# num = int(input("Ingresa un numero: "))
+
+# while num < 0:
+#     num = int(input("Ingresa un numero: "))
+#     print("El numero debe ser positivo")

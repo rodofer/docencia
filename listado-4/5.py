677d5922aa255750cefc246dd9411e4a53218ec1
Rodolfo <rodofercl@gmail.com>
2024-09-11 17:20:02 -0300

    ejercicio 5

---
+++ listado-4/5.py
@@ -0,0 +1,17 @@
+"""
+Solicitar números desde el teclado hasta que el 
+usuario ingrese el valor cero. Luego, mostrar por
+pantalla el promedio de los números positivos
+"""
+
+sumatoria=0
+contador=0
+
+while True:
+    numero=int(input("ingrese sus numeros para pomediarlo: "))
+    if numero == 0:
+        break
+    sumatoria+=numero
+    contador+=1
+
+print(f"su promedio: {sumatoria/contador:.1f}")
